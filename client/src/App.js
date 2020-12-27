89593b610b44b55e8c99c38726d4aefd09a6e419
Ankit Hans <ankithans1947@gmail.com>
2020-12-27 21:42:12 +0530

    fix(api): fix client build script

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,12 @@ import "./tailwind.output.css";
 
 function App() {
   return (
-    <div className='p-6 m-4 bg-black w-40 text-red-300 rounded-lg'>
-      Hello tailwind
-    </div>
+    <>
+      <div className='p-6 m-4 bg-black w-40 text-red-300 rounded-lg'>
+        Hello tailwind
+      </div>
+      <h1>wlcnenen</h1>
+    </>
   );
 }
 
