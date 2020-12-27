73e087512e03ac8a574c2b6b586682b1ddb7a3a1
Ankit Hans <ankithans1947@gmail.com>
2020-12-27 20:41:48 +0530

    feat(*): prepare for deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import "./tailwind.output.css";
 
 function App() {
-  return <>Hello</>;
+  return (
+    <div className='p-6 m-4 bg-black w-40 text-red-300 rounded-lg'>
+      Hello tailwind
+    </div>
+  );
 }
 
 export default App;
