147bf9ef01e28f455012e51061b8d439b5628642
Ankit Hans <ankithans1947@gmail.com>
2020-12-30 20:50:45 +0530

    feat(client/context): admin get all employees context

---
+++ client/src/context/Admin/AdminProvider.js
@@ -0,0 +1,70 @@
+import React, { createContext, useReducer } from "react";
+import axios from "axios";
+import AdminReducer from "./AdminReducer";
+import { useToasts } from "react-toast-notifications";
+
+// Initial state
+const initialState = {
+  isLoggedIn: localStorage.getItem("isLoggedIn") === "true" ? true : false,
+  loading: false,
+  employees: [],
+};
+
+// create context
+export const AdminContext = createContext(initialState);
+
+// Provider component
+export const AdminProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AdminReducer, initialState);
+  const { addToast } = useToasts();
+
+  // actions
+  async function getAllEmployees() {
+    try {
+      dispatch({
+        type: "EMPLOYEES_LOADING",
+      });
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          "x-auth-token": localStorage.getItem("token"),
+        },
+      };
+      const res = await axios.get("/api/v1/admin/getAllEmployees", config);
+
+      console.log(res);
+
+      addToast("Employee Data loaded Successfully!", {
+        appearance: "success",
+        autoDismiss: true,
+      });
+
+      dispatch({
+        type: "EMPLOYEES_LOADED",
+        payload: res.data.employees,
+      });
+    } catch (err) {
+      addToast(err.response.data.error, {
+        appearance: "error",
+        autoDismiss: true,
+      });
+
+      dispatch({
+        type: "ERROR",
+        payload: err.response.data.error,
+      });
+    }
+  }
+
+  return (
+    <AdminContext.Provider
+      value={{
+        isLoggedIn: state.isLoggedIn,
+        loading: state.loading,
+        getAllEmployees,
+      }}
+    >
+      {children}
+    </AdminContext.Provider>
+  );
+};
---
+++ client/src/context/Admin/AdminReducer.js
@@ -0,0 +1,23 @@
+// eslint-disable-next-line
+export default (state, action) => {
+  switch (action.type) {
+    case "EMPLOYEES_LOADING":
+      return {
+        ...state,
+        loading: true,
+      };
+    case "EMPLOYEES_LOADED":
+      return {
+        ...state,
+        loading: false,
+        employees: action.payload,
+      };
+    case "ERROR":
+      return {
+        ...state,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+};
