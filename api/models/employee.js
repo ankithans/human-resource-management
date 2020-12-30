ecc8efee0f58c5ed3791865f11ad5101e78fd461
Ankit Hans <ankithans1947@gmail.com>
2020-12-30 18:03:37 +0530

    feat(api/models): add employee model

---
+++ api/models/employee.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const EmployeeSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  team: {
+    type: String,
+    required: true,
+  },
+  salary: {
+    type: Number,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Admin", EmployeeSchema);
